da949b6085ae68d491fc3b7350a5e56542fbf6b0
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2024-09-16 21:38:34 +0530

    Create LongestValidParentheses.cpp
    16th September, 2024

---
+++ LongestValidParentheses.cpp
@@ -0,0 +1,21 @@
+int maxLength(string& s) {
+        // code here
+        int n = s.length();
+        stack<int> st;
+        st.push(-1);
+        int max_l = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    max_l = max(max_l, i - st.top());
+                }
+            }
+        }
+    return max_l;
+}
