92e92a67d2ec28068e063fca9133cec18fba7ce5
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2024-10-20 21:59:00 +0530

    20th October, 2024

---
+++ SortAKSortedDoublyLinkedList.cpp
@@ -0,0 +1,22 @@
+DLLNode *sortAKSortedDLL(DLLNode *head, int k) {
+        DLLNode *temp = head;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        while(temp && k--){
+            pq.push(temp->data);
+            temp = temp->next;
+        }
+        
+        DLLNode *curr = head;
+        while(curr){
+            if(temp){
+                pq.push(temp->data);
+                temp=temp->next;
+            }
+            
+            curr->data = pq.top();
+            pq.pop();
+            curr = curr->next;
+        }
+        
+        return head;
+    }
