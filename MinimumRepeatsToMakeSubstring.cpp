1f7137868363bb61ae2f60c884beb22f4a40deae
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2024-11-08 23:52:04 +0530

    8th November, 2024

---
+++ MinimumRepeatsToMakeSubstring.cpp
@@ -0,0 +1,15 @@
+int minRepeats(string& A, string& B) {
+        // code here
+        string rep = A;
+        int cnt=1;
+        while(A.size()<B.size()){
+            A+=rep;
+            cnt++;
+        }
+       
+        if(A.find(B)!=-1) return cnt;
+         A+=rep;
+         cnt++;
+        if(A.find(B)!=-1) return cnt;
+        return -1;
+    }
