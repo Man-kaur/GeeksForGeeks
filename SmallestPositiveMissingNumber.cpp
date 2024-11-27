d7cf6356d4d6dc9dba6d4c9446ff0df183c03031
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2024-11-27 18:19:47 +0530

    27th November, 2024

---
+++ SmallestPositiveMissingNumber.cpp
@@ -0,0 +1,17 @@
+int missingNumber(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        
+        for(int i=0; i<n ;i++){
+            while(arr[i]>0 && arr[i]<=n && arr[i]!=arr[arr[i]-1]){
+                swap(arr[i], arr[arr[i]-1]);
+            }
+        }
+        
+        
+        for(int i=0 ;i<n;i++){
+            if(arr[i] != i+1) return i+1;
+        }
+        
+        return n+1;
+    }
