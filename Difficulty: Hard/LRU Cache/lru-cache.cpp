0f2376c9a3175f76181066aadbc29f55e663b4a9
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-01-27 19:25:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/LRU Cache/lru-cache.cpp
@@ -0,0 +1,135 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// design the class in the most optimal way
+
+class Node{
+    public:
+    int key,val;
+    Node*next;
+    Node*prev;
+    
+    Node(){
+        
+        key=val=-1;
+        
+        next=prev=NULL;
+    }
+    
+    Node(int k,int value){
+        key = k;
+        val = value;
+        next = prev = NULL;
+    }
+};
+
+class LRUCache {
+    
+  private:
+  unordered_map<int,Node*>mp;
+  int capacity;
+  Node*head;
+  Node*tail;
+  void insertAfterHead(Node*node){
+      Node*nextNode = head->next;
+      head->next = node;
+      node->prev = head;
+      node->next = nextNode;
+      nextNode->prev = node;
+     
+  }
+  void deleteNode(Node*node){
+      Node*before = node->prev;
+      Node*after = node->next;
+      before->next = after;
+      after->prev = before;
+     
+      
+  }
+  
+  public:
+    // Constructor for initializing the cache capacity with the given value.
+    
+    LRUCache(int cap) {
+        // code here
+        capacity = cap;
+        mp.clear();
+        head = new Node();
+        tail = new Node();
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    // Function to return value corresponding to the key.
+    int get(int key) {
+        // your code here
+        if(mp.find(key)==mp.end()) return -1;
+         Node*node = mp[key];
+         int val = node->val;
+         deleteNode(node);
+         insertAfterHead(node);
+         return val;
+    }
+
+    // Function for storing key-value pair.
+    void put(int key, int value) {
+        // your code here
+        if(mp.find(key)!=mp.end()){
+            Node*node = mp[key];
+            node->val = value;
+            deleteNode(node);
+            insertAfterHead(node);
+            return ;
+        }
+        
+        if(mp.size()==capacity){
+            Node*NodeTodelete = tail->prev;
+            mp.erase(NodeTodelete->key);
+            deleteNode(NodeTodelete);
+            delete NodeTodelete;
+        }
+        
+        Node*newNode = new Node(key,value);
+        mp[key]= newNode;
+        insertAfterHead(newNode);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int capacity;
+        cin >> capacity;
+        LRUCache *cache = new LRUCache(capacity);
+
+        int queries;
+        cin >> queries;
+        while (queries--) {
+            string q;
+            cin >> q;
+            if (q == "PUT") {
+                int key;
+                cin >> key;
+                int value;
+                cin >> value;
+                cache->put(key, value);
+            } else {
+                int key;
+                cin >> key;
+                cout << cache->get(key) << " ";
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
