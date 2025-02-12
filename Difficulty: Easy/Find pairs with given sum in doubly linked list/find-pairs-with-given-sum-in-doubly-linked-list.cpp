cda8b61afef22ff447c13929de692f502674ff89
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-02-12 22:49:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+        vector<pair<int, int>> res;
+        Node* low = head, *high = head;
+        int i=0, j=0;
+        
+        while(high->next) high = high->next, j++;
+        
+        while(low && high && i < j){
+            int sum = low->data + high->data;
+            
+            if(sum==target){
+                res.push_back({low->data, high->data});
+                low = low->next;
+                high = high->prev;
+                i++;
+                j--;
+            }else if(sum<target){
+                low = low->next;
+                i++;
+            }else{
+                high = high->prev;
+                j--;
+            }
+        }
+        
+        return res;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, target;
+        cin >> target >> n;
+        int a;
+        cin >> a;
+        Node *head = new Node(a);
+        Node *tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> a;
+            tail->next = new Node(a);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+        Solution ob;
+        auto ans = ob.findPairsWithGivenSum(head, target);
+        if (ans.size() == 0)
+            cout << "-1";
+        else
+        {
+            for (int i = 0; i < ans.size(); i++)
+            {
+                cout << "(" << ans[i].first << "," << ans[i].second << ")"
+                     << " ";
+            }
+        }
+        cout << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
