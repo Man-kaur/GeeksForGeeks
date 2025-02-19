0e941a41d267aed4caf60339f2c0459dc83e0d7c
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-02-19 23:16:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+// C++ program to merge k sorted arrays of size n each
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List node
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to print nodes in a given linked list */
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*Linked list Node structure
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    Node* mergeKLists(vector<Node*>& arr) {
+        // Your code here
+        
+        int n = arr.size();
+        vector<int> num;
+        
+        for(int i=0; i<n; i++){
+            Node* temp = arr[i];
+            while(temp){
+                num.push_back(temp->data);
+                temp = temp->next;
+            }
+        }
+        sort(num.begin(), num.end());
+        
+         Node* head = new Node(num[0]);
+        Node* curr = head;
+        for(int i=1; i<num.size(); i++){
+            Node* node = new Node(num[i]);
+            curr->next = node;
+            curr = curr->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test the above functions
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n;
+        cin >> n;
+        cin.ignore();
+
+        vector<Node*> v(n);
+
+        for (int i = 0; i < n; i++) {
+            string line;
+            getline(cin, line);
+            stringstream ss(line);
+
+            Node* head = new Node(0);
+            Node* temp = head;
+            int x;
+            while (ss >> x) {
+                Node* newNode = new Node(x);
+                temp->next = newNode;
+                temp = temp->next;
+            }
+            v[i] = head->next;
+        }
+
+        Solution ob;
+        Node* head = ob.mergeKLists(v);
+        printList(head);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
