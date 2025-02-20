e5ea60142cc9866a36166a180cc400807c88eae7
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-02-20 10:16:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Graph and Vertices/graph-and-vertices.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long count(int n) {
+        // your code here
+        long long a = n * (n-1) / 2;
+        return pow(2, a);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.count(n) << "\n";
+    
+cout << "~" << "\n";
+}
+
+    return 0;
+}
+
+// } Driver Code Ends
